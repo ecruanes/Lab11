f89262024d008bfb2580be147254ba5f2b656300
ecruanes <eve.cruanes@gmail.com>
2023-11-29 12:39:30 -0500

    Lab8 Final Version
    Using the copilot version, completed the library class and tested it.

--- Lab8code/Library.cpp
+++ Lab8code/Library.cpp
@@ -1,3 +1,52 @@
 //include headers and libraries
+#include "Library.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
 
 //implement class functions
+Library::Library(){
+    //initialize all books to ""
+    for(int i = 0; i < 10; i++){
+        books[i] = "";
+    }
+}
+bool Library:: addBook(string bookName){
+    //check if the book already exists
+    for(int i = 0; i < 10; i++){
+        if(books[i] == bookName){
+            return false;
+        }
+    }
+    //check if the library is full
+    for(int i = 0; i < 10; i++){
+        if(books[i] == ""){
+            books[i] = bookName;
+            return true;
+        }
+    }
+    return false;
+}
+bool Library::removeBook(string bookName){
+    //check if the book exists
+    for(int i = 0; i < 10; i++){
+        if(books[i] == bookName){
+            books[i] = "";
+            return true;
+        }
+    }
+    cout << "Cannot remove the book." << endl;
+    return false;
+}
+void Library::print(){
+    //print all books
+    for(int i = 0; i < 10; i++){
+        if(books[i] != ""){
+            cout << books[i] << endl;
+        }
+    }
+}  //end of Library.cpp
--- Lab8code/P1.cpp
+++ Lab8code/P1.cpp
@@ -1,8 +1,46 @@
 //include headers and libraries
+#include "Library.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
 
 int main(){
     Library library;
+    char command;
+    string bookname; 
+    while(true){ 
+        cout << "Enter command: ";
+        cin >> command;
+        //ask for commands and execute
+        if(command == 'q'){
+            cout<<"Quitting"<<endl; 
+            break;
+        }
+        else if(command == 'a'){
+            cout << "Enter book name: ";
+            cin >> bookname;
+            library.addBook(bookname);
+        }
+        else if(command == 'r'){
+            cout << "Enter book name: ";
+            cin >> bookname;
+            library.removeBook(bookname);
+        }
+        else if(command == 'p'){
+            library.print();
+        }
+        else{
+            cout << "Invalid command" << endl;
+        }
+
+    }
+    return 0;
     
-    //ask for commands and execute
+    
 
 }
